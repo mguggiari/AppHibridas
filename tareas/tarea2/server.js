fa27250d232ae84ef8812e55ae1ce4be0b7a3941
Micaela Guggiari <102701164+mguggiari@users.noreply.github.com>
2023-03-20 20:07:59 -0300

    correcciones en clase

--- tareas/tarea2/server.js
+++ tareas/tarea2/server.js
@@ -2,15 +2,17 @@ const http = require('node:http');
 const server = http.createServer (function(request, response){
     //console.log('Hola te estoy escuchando');
     //response.write("Hola mundo");
+    response.write('<h1>Mi espectacular pagina web!</h1>');
+    response.write ('<!DOCTYPE html><html><head><meta charset="UTF-8"><title>App Hibridas</title></head><body>');
     console.log('url: ' + request.url);
     if(request.url == "/") {
-        response.write("Micaela Guggiari");
+        response.write('<p>Micaela Guggiari</p>');
     } 
     else if (request.url == "/materia"){
-        response.write("App Hibridas");
+        response.write('<p>App Hibridas</p>');
     }
     else if (request.url == "/profesor"){
-        response.write("Brian Campos");
+        response.write('<p>Brian Campos</p>');
     }
     else if(request.url == "/productos") {
         let productos = [];
@@ -41,12 +43,16 @@ const server = http.createServer (function(request, response){
                 precio: 200
             }
         ]
-        response.write(JSON.stringify(productos));
+        response.write('<ul>')
+        for (let i = 0; i < productos.length; i++) {
+            response.write('<li>' + productos[i].nombre + ' - $' + productos[i].precio + '</li>')
+        }
+        response.write('</ul>')
     }
     else {
-        response.write("Error 404 url no encontrada :(");
+        response.write('<p>Error 404 url no encontrada :(</p>');
     }
 
     response.end();
 })
-server.listen(2025);
+server.listen(2022);
