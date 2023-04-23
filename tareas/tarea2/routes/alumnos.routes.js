4e52012dcb5e159dcbde27762757d005c68c7305
mguggiari <micaela.guggiari@davinci.edu.ar>
2023-04-23 13:41:41 -0300

    alumnos.routes.js

--- tareas/tarea2/routes/alumnos.routes.js
+++ tareas/tarea2/routes/alumnos.routes.js
@@ -4,8 +4,8 @@ import * as controller from '../controllers/alumnos.controllers.js';
 
 const route = express.Router();
 
-route.get('/', controller.getAlumnos)
-route.get('/alumnos', controller.getAlumnos)
-route.get('/alumnos/:legajoAlumno', controller.getAlumnoByLegajo)
+// route.get('/', controller.getAlumnos)
+// route.get('/alumnos', controller.getAlumnos)
+// route.get('/alumnos/:legajoAlumno', controller.getAlumnoByLegajo)
 
 export default route
