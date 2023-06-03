9e20342e34246f2075f3487d2af66be2c051afa3
mguggiari <micaela.guggiari@davinci.edu.ar>
2023-06-03 20:45:05 -0300

    clase 09/05
    yup: schema

---
+++ clases/clase18/schemas/products.schemas.js
@@ -0,0 +1,21 @@
+import yup from 'yup'
+
+const productSchema = yup.object({
+    name: yup.string().required(),
+    //msj de error se puede customizar ahí adentro
+    price: yup.number().required("Pone el precio, gil!").positive(),
+    description: yup.string().required(),
+    tags: yup.array().of(yup.string()).required()
+})
+
+const productUpdateSchema = yup.object({
+    name: yup.string(),
+    price: yup.number().positive(),
+    description: yup.string(),
+    tags: yup.array().of(yup.string())
+})
+
+export {
+    productSchema,
+    productUpdateSchema
+}
